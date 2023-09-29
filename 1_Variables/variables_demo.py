331c14eac14a83f915f8af2bf9296ae274385011
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2023-09-29 17:30:36 +0600

    Variables,Operators

---
+++ 1_Variables/variables_demo.py
@@ -0,0 +1,45 @@
+"""
+Data Types:
+1. Numeric(integer, float)
+2. Sequence(string, list, tuple)
+3. Boolean
+4. Mapping
+5. Set
+"""
+number2 = 10
+tax = 10.3
+
+# Sequence(string, list, tuple)
+name = 'Mr.Smith'
+email = "test@mail.com"
+
+product_id = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # list
+employee_id = (1, 2, 3, 4, 5)  # tuple
+
+# Boolean
+login_status = True
+active = True
+
+# Mapping(Dictionary)
+# key : Value
+user_details = {
+    "name": "Mr. Smith",
+    "email": "test@mail.com",
+    "active": True,
+    "phone": 1233,
+    "passwords": [123, 1234, 324324, 333],
+    "address": {
+        "address_present": {
+            "present_address": "Dhaka",
+            "road": 12
+        },
+        "address_permanent": {
+            "permanent_address": "NY",
+            "road": 1234
+        }
+    }
+}
+
+# Set
+group1 = {12, 33, 44, 43}
+group2 = {23, 34, 543}
---
+++ 2_Operators/operators_demo.py
@@ -0,0 +1,43 @@
+"""
+1. Arithmetic
+2. Logical
+3. Assignment
+4. Comparison
+"""
+
+# Arithmetic: + - * / % // **
+print(5 ** 2)
+
+# Comparison
+"""
+'==' Equal to 
+'!=' Not equal to
+'<' Less than
+'>' Greater than
+'<=' Less than or equal to
+'>=' Greater or equal to
+"""
+
+print(10 >= 10)  # True
+
+"""
+Assignment:
+'=' (Assignment)
+'+=' (Add and Assign)
+'-=' (Subtract and Assign)
+'*=' (Multiply and Assign)
+'/=' (Divide and Assign)
+'%=' (Modulo and Assign)
+'//=' (Floor and Assign)
+"""
+
+"""
+Logical:
+and
+or
+not
+"""
+
+print(10 == 10 and 10 != 10)  # False
+print(10 == 10 or 10 != 10)  # True
+print(not (10 == 10 or 10 != 10))
